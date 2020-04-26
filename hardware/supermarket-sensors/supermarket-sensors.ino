f6449ec160860c9dff198e134352524c6a2251d7
chronoclast <a.lie.called.life@gmail.com>
2020-04-26 05:01:32 +0200

    New version supporting two sensors.

--- hardware/supermarket-sensors/supermarket-sensors.ino
+++ hardware/supermarket-sensors/supermarket-sensors.ino
@@ -1,5 +1,6 @@
 /* ----------------------------------------------------------------------------
-PROJECT: Hello(NotSo)Fresh | EarthxHack online event (April 24-26, 2020)
+PROJECT:
+  Hello(NotSo)Fresh | EarthxHack online event (April 24-26, 2020)
 DESCRIPTION:
   Supermarket monitoring sensors to keep track of the stocks and the shelf time
   of each product. The data is sent regularly to the Adafruit IO platform.
@@ -8,6 +9,9 @@ DESCRIPTION:
 CHANGELOG:
   0.0 | Tracking stock and shelf time for one product (in the middle of the
       | shelf).
+  1.0 | Added one more sensor (right of the shelf). Tracking two products now.
+  1.1 | Publishing only the shelf times, the lines that apply to the stocks
+      | have been commented. 
 ---------------------------------------------------------------------------- */
 
 #include "config.h"
@@ -18,13 +22,18 @@ unsigned long lastUpdate = 0;
 // Variables to store the stocks and the shelf times locally.
 bool stock_middle = false;
 int time_middle = 0;
+bool stock_right = false;
+int time_right = 0;
 
 // Set up the feeds.
-AdafruitIO_Feed *stock_middle_feed = io.feed("stock_middle");
+// AdafruitIO_Feed *stock_middle_feed = io.feed("stock_middle");
 AdafruitIO_Feed *time_middle_feed = io.feed("time_middle");
+// AdafruitIO_Feed *stock_right_feed = io.feed("stock_right");
+AdafruitIO_Feed *time_right_feed = io.feed("time_right");
 
 // Pin for the ultrasound sensor.
-const int pingPin = 4;
+const int ping_pin = 4;
+const int lux_pin = 34;
 
 void setup() {
 
@@ -45,43 +54,51 @@ void setup() {
   // Connection successful; print the status message and get the feeds.
   Serial.println();
   Serial.println(io.statusText());
-  stock_middle_feed->get();
+  // stock_middle_feed->get();
+  time_middle_feed->get();
+  // stock_right_feed->get();
+  time_right_feed->get();
 
 }
 
 void loop() {
 
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
+  // This function keeps the client connected to the Adafruit platform.
+  // It must be always at the top of the main loop.
   io.run();
 
-  // Variables for the duration of the "ping" and the distance in cm.
-  long duration;
-  int cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+  // --- LOGIC FOR THE MIDDLE SHELF (using the ultrasound sensor) -------------
+  // --------------------------------------------------------------------------
+  // Variables for the duration of the "ping" and the distance in cm, used by
+  // the ultrasound sensor.
+  long duration;
+  int distance_cm;
+
+  // Variable to return the reading of the ADC, provided by the light sensor.
+  float adc_voltage_lux;
+
+  // The ultrasound sensor is triggered by a HIGH pulse of 2 or more
+  // microseconds. A short LOW pulse beforehand ensures a clean HIGH pulse.
+  pinMode(ping_pin, OUTPUT);
+  digitalWrite(ping_pin, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(ping_pin, HIGH);
   delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(ping_pin, LOW);
 
   // The same pin is used to read the signal from the PING))): a HIGH pulse
   // whose duration is the time (in microseconds) from the sending of the ping
   // to the reception of its echo off of an object.
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+  pinMode(ping_pin, INPUT);
+  duration = pulseIn(ping_pin, HIGH);
 
   // Convert the time into distance in cm.
-  cm = microsecondsToCentimeters(duration);
+  distance_cm = microsecondsToCentimeters(duration);
 
-  // If a bag is detected in the middle of the shelf, it that there's stock.
-  // Otherwise 
-  if (cm <= 25) {
+  // If a bag is detected in the middle of the shelf, it means that there's
+  // still enough in stock. Otherwise, the stock_middle flag will be toggled.
+  if (distance_cm <= 25) {
     stock_middle = true;
   } else {
     stock_middle = false;
@@ -89,30 +106,66 @@ void loop() {
   }
 
   Serial.print("[DEBUG] Distance (cm): ");
-  Serial.println(cm);
+  Serial.println(distance_cm);
+  // --------------------------------------------------------------------------
 
+
+  // --- LOGIC FOR THE RIGHT SHELF (using the luminosity sensor) --------------
+  // --------------------------------------------------------------------------
+  adc_voltage_lux = readAPDS9002Vout(lux_pin);
+
+  // If there's produce in the right of the shelf, the bottom of the wooden box
+  // will get darker. This means that there's still enough in stock of this
+  // particular product. Otherwise, the stock_right flag will be toggled.
+  if (adc_voltage_lux <= 0.10) {
+    stock_right = true;
+  } else {
+    stock_right = false;
+    time_right = 0;
+  }
+
+  Serial.print("[DEBUG] Luminosity, ADC Vout (V): ");
+  Serial.println(adc_voltage_lux);
+  // --------------------------------------------------------------------------
+
+
+  // Values will be collected every second.
   delay(1000);
 
-
   if (millis() > (lastUpdate + IO_LOOP_DELAY)) {
-    // Publish the feeds onto the Adafruit platform.
+    // Publish the feeds onto the Adafruit platform, first the middle stock.
     Serial.print("[INFO] Middle stock: ");
     if (stock_middle == true) {
       Serial.println("YES");
     } else {
       Serial.println("NO");
     }
-    stock_middle_feed->save(stock_middle);
+
+    // stock_middle_feed->save(stock_middle);
     Serial.print("[INFO] Middle shelf time: ");
     Serial.println(time_middle);
     time_middle_feed->save(time_middle);
+
+    // Then the right stock.
+    Serial.print("[INFO] Right stock: ");
+    if (stock_right == true) {
+      Serial.println("YES");
+    } else {
+      Serial.println("NO");
+    }
+
+    // stock_right_feed->save(stock_right);
+    Serial.print("[INFO] Right shelf time: ");
+    Serial.println(time_right);
+    time_right_feed->save(time_right);
 
     // After publishing, store the current time.
     lastUpdate = millis();
     
     // Increase the shelf time of all products according to the loop delay.
-    // This one is given in ms, so it's converted to s.
+    // This one is given in milliseconds, so it's converted to seconds.
     time_middle += IO_LOOP_DELAY/1000;
+    time_right += IO_LOOP_DELAY/1000;
   }
 
 }
@@ -122,4 +175,15 @@ long microsecondsToCentimeters(long microseconds) {
   // The ping travels out and back, so to find the distance of the object we
   // take half of the distance travelled.
   return microseconds / 29 / 2;
+}
+
+
+float readAPDS9002Vout(uint8_t analogpin) {
+    // MeasuredVout = ADC Value * (Vcc / 1023) * (3 / Vcc)
+    // Vout samples are with reference to 3V Vcc
+    // The above expression is simplified by cancelling out Vcc
+    float MeasuredVout = analogRead(analogpin) * (3.0 / 1023.0);
+    // Above 2.3V , the sensor value is saturated
+
+    return MeasuredVout;
 }
